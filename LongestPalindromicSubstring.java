2e642842d54769890f98ddcdc2bc6fdacda5fedb
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-11-09 21:17:18 +0530

    changes

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -1,8 +1,15 @@
 public class LongestPalindromicSubstring {
 
     public static void main(String[] args) {
-        String subString = "babad";
+        String string = "babad";
+        longestPalindromicSubstring(string);
         // output "bab"
+
+    }
+
+    static void longestPalindromicSubstring(String s) {
+        // loop make a = b then run it once find if b exit twice if yes then take it if
+        // not then lose first lettle and start over
 
     }
 }
