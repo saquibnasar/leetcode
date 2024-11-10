f0a56746041e522494cd21f1523b7dcf1f4632ab
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-11-10 23:44:37 +0530

    part substring that reads the same backward as forward

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -1,15 +1,41 @@
 public class LongestPalindromicSubstring {
 
     public static void main(String[] args) {
-        String string = "babad";
-        longestPalindromicSubstring(string);
+        String string = "cbd";
+
+        String ans = longestPalindromicSubstring(0, 1, string);
+        System.out.println(ans);
         // output "bab"
+
+        // https://leetcode.com/problems/longest-palindromic-substring/description/
 
     }
 
-    static void longestPalindromicSubstring(String s) {
-        // loop make a = b then run it once find if b exit twice if yes then take it if
-        // not then lose first lettle and start over
+    static String longestPalindromicSubstring(int curret, int check, String s) {
 
+        while (curret < check) {
+            if (s.charAt(curret) == s.charAt(check)) {
+                Boolean ans = palindromic(curret + 1, check - 1, s);
+                if (ans) {
+                    return s.substring(curret, check + 1);
+                }
+            }
+            check++;
+            if (check == s.length()) {
+                curret++;
+                check = curret + 1;
+            }
+        }
+        return "";
+    }
+
+    static boolean palindromic(int start, int end, String word) {
+        if (start > end) {
+            return true;
+        }
+        if (word.charAt(start) == word.charAt(end)) {
+            return palindromic(start + 1, end - 1, word);
+        }
+        return false;
     }
 }
