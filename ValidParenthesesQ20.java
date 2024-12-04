429739ee9d98d7b2760c426ebbc2d5e567b7d5e3
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-12-05 01:34:00 +0530

    Valid Parentheses

--- ValidParenthesesQ20.java
+++ ValidParenthesesQ20.java
@@ -1,47 +1,44 @@
 public class ValidParenthesesQ20 {
     public static void main(String[] args) {
-        String s = "([])";
+        String s = "(]";
         boolean ans = isValid(s);
         System.out.println(ans);
     }
 
     public static boolean isValid(String s) {
+
         if (s.length() == 0) {
             return true;
         }
-
+        boolean ans = true;
         for (int i = 0; i < s.length(); i++) {
-            for (int j = 1; j < s.length(); j++) {
+
+            for (int j = i + 1; j < s.length(); j++) {
                 if (s.charAt(i) == '(') {
                     if (s.charAt(j) == ')') {
+                        ans = true;
                         break;
-                    } else if (s.charAt(j) == '}') {
-                        return false;
-                    } else if (s.charAt(j) == ']') {
-                        return false;
-
                     }
                 } else if (s.charAt(i) == '{') {
-                    if (s.charAt(j) == ')') {
-                        break;
-                    } else if (s.charAt(j) == '}') {
-                        break;
-                    } else if (s.charAt(j) == ']') {
-                        return false;
-                    }
-                }
-
-                else if (s.charAt(i) == '[') {
-                    if (s.charAt(j) == ')') {
-                        break;
-                    } else if (s.charAt(j) == '}') {
-                        break;
-                    } else if (s.charAt(j) == ']') {
+                    if (s.charAt(j) == '}') {
+                        ans = true;
                         break;
                     }
+                } else if (s.charAt(i) == '[') {
+                    if (s.charAt(j) == ']') {
+                        ans = true;
+                        break;
+                    }
+                } else {
+                    break;
                 }
+                ans = false;
             }
+            if (!ans) {
+                break;
+            }
+
         }
-        return true;
+        return ans;
     }
 }
