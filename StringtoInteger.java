67cb14db613d41691aa9cbeafaf343a5e97c482c
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-12-06 00:52:57 +0530

    converting string to integer

--- StringtoInteger.java
+++ StringtoInteger.java
@@ -1,25 +1,24 @@
 public class StringtoInteger {
 
     public static void main(String[] args) {
-        String str = "0-1";
-
+        String str = " -042";
         System.out.println(myAtoi(str));
     }
 
     public static int myAtoi(String str) {
         str = str.replaceAll("\\s", "");
+        // 48 to 58 0 to 9
+        // 65 to 90 A to Z
+        // 97 to 122 a to z
         int ans = 0;
-        boolean isNegative = false;
 
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '-') {
-                isNegative = true;
-            } else if (ans == 0 && str.charAt(i) == '0') {
-                if (str.charAt(i + 1) == '-') {
-                    return 0;
-                }
-                continue;
-            } else {
+            if (0 + str.charAt(i) >= 65) {
+                return ans;
+            }
+            if (ans == 0 && str.charAt(i) == '0' && (str.charAt(i + 1) == '-' || str.charAt(i + 1) == '+')) {
+                return 0;
+            } else if (0 + str.charAt(i) >= 48 && 0 + str.charAt(i) <= 57) {
                 if (ans == 0) {
                     ans = ans + str.charAt(i) - '0';
                 } else {
@@ -27,7 +26,7 @@ public class StringtoInteger {
                 }
             }
         }
-        if (isNegative) {
+        if (str.charAt(0) == '-') {
             return -ans;
         }
         return ans;
