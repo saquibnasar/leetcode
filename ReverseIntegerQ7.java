cab747a6f752b9c53bc380e8fcb21967b71ae51a
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-12-29 10:27:59 +0530

    reverse integer using just int_max and int_Min

--- ReverseIntegerQ7.java
+++ ReverseIntegerQ7.java
@@ -8,20 +8,38 @@ public class ReverseIntegerQ7 {
 
     public static int reverse(int x) {
         int rev = 0;
-        long ifOutRange = 0L;
 
         int INT_MIN = Integer.MIN_VALUE; // -2147483648
         int INT_MAX = Integer.MAX_VALUE; // 2147483647
 
         while (x != 0) {
-            rev = rev * 10 + x % 10;
-            ifOutRange = (ifOutRange * 10 + x % 10) * 1L;
+            int digit = x % 10;
             x = x / 10;
-            if (ifOutRange > INT_MAX || ifOutRange < INT_MIN) {
+            if (rev > INT_MAX / 10 || rev == INT_MAX / 10 && digit >= INT_MAX % 10) {
+                return 0;
+            } else if (rev < INT_MIN / 10 || rev == INT_MIN / 10 && digit <= INT_MIN % 10) {
                 return 0;
             }
-
+            rev = rev * 10 + digit;
         }
         return rev;
     }
+    // public static int reverse(int x) {
+    // int rev = 0;
+    // long ifOutRange = 0L;
+
+    // int INT_MIN = Integer.MIN_VALUE; // -2147483648
+    // int INT_MAX = Integer.MAX_VALUE; // 2147483647
+
+    // while (x != 0) {
+    // rev = rev * 10 + x % 10;
+    // ifOutRange = (ifOutRange * 10 + x % 10) * 1L;
+    // x = x / 10;
+    // if (ifOutRange > INT_MAX || ifOutRange < INT_MIN) {
+    // return 0;
+    // }
+
+    // }
+    // return rev;
+    // }
 }
