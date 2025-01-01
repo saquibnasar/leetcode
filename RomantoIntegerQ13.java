9a361a72776cbfb01aedf7835709512d7e6a6662
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-01-01 14:18:38 +0530

    roman to integer using if else

--- RomantoIntegerQ13.java
+++ RomantoIntegerQ13.java
@@ -1,37 +1,48 @@
 public class RomantoIntegerQ13 {
     public static void main(String[] args) {
-        String roman = "MCMXCIV";
+        String roman = "DCXXI";
 
         System.out.println(romanToInt(roman));
 
     }
 
     public static int romanToInt(String s) {
+        if (s.length() == 0)
+            return 0;
         int ans = 0;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             int value = 0;
             if (c == 'I') {
-                c = s.charAt(i + 1);
-                if (c == 'V') {
-                    value = 4;
-                    i++;
-                } else if (c == 'X') {
-                    value = 9;
-                    i++;
+                if (i + 1 < s.length()) {
+                    c = s.charAt(i + 1);
+                    if (c == 'V') {
+                        value = 4;
+                        i++;
+                    } else if (c == 'X') {
+                        value = 9;
+                        i++;
+                    } else {
+                        value = 1;
+                    }
                 } else {
                     value = 1;
                 }
+
             } else if (c == 'V') {
                 value = 5;
             } else if (c == 'X') {
-                c = s.charAt(i + 1);
-                if (c == 'L') {
-                    value = 40;
-                    i++;
-                } else if (c == 'C') {
-                    value = 90;
-                    i++;
+                if (i + 1 < s.length()) {
+                    c = s.charAt(i + 1);
+                    if (c == 'L') {
+                        value = 40;
+                        i++;
+                    } else if (c == 'C') {
+                        value = 90;
+                        i++;
+                    } else {
+                        value = 10;
+                    }
                 } else {
                     value = 10;
                 }
@@ -39,17 +50,20 @@ public class RomantoIntegerQ13 {
             } else if (c == 'L') {
                 value = 50;
             } else if (c == 'C') {
-                c = s.charAt(i + 1);
-                if (c == 'D') {
-                    value = 400;
-                    i++;
-                } else if (c == 'M') {
-                    value = 900;
-                    i++;
+                if (i + 1 < s.length()) {
+                    c = s.charAt(i + 1);
+                    if (c == 'D') {
+                        value = 400;
+                        i++;
+                    } else if (c == 'M') {
+                        value = 900;
+                        i++;
+                    } else {
+                        value = 100;
+                    }
                 } else {
                     value = 100;
                 }
-
             } else if (c == 'D') {
                 value = 500;
             } else if (c == 'M') {
