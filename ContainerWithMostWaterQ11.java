9c88e8747329f9e032ce77457a7acc6b531873f9
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-01-06 12:10:50 +0530

    tring Container WithMost Water

--- ContainerWithMostWaterQ11.java
+++ ContainerWithMostWaterQ11.java
@@ -8,35 +8,40 @@ public class ContainerWithMostWaterQ11 {
         if (height.length == 0) {
             return 0;
         }
-        int left = 1;
-        int right = height.length - 2;
-        int leftMax = 0;
-        int rightMax = height.length - 1;
+        int left = height.length / 2 - 2;
+        int right = height.length / 2 + 1;
+        int leftMax = height.length / 2 - 1;
+        int rightMax = height.length / 2;
 
-        while (left < height.length && right >= 0) {
+        while (left >= 0 || right < height.length) {
+
+            // left
+            if (left >= 0 && height[rightMax] >= height[left]) {
+                if (height[left] + leftMax - left >= height[leftMax]) {
+                    leftMax = left;
+                }
+            } else if (left >= 0 && height[rightMax] < height[left]) {
+                if (height[left] + leftMax - left >= height[leftMax]) {
+                    leftMax = left;
+                }
+                // if (height[left] - (height[left] - height[rightMax]) + leftMax - left >=
+                // height[leftMax]) {
+                // leftMax = left;
+                // }
+            }
 
             // for right
-            if (height[leftMax] >= height[right]) {
-                if (height[right] > height[rightMax] + (rightMax - right)) {
+            if (right < height.length && height[leftMax] >= height[right]) {
+                if (height[right] + right - rightMax >= height[rightMax]) {
                     rightMax = right;
                 }
-            } else if (height[leftMax] < height[right]) {
-                if (height[leftMax] > height[rightMax] + (rightMax - right)) {
+            } else if (right < height.length && height[leftMax] < height[right]) {
+                if (height[right] + rightMax - right >= height[rightMax]) {
                     rightMax = right;
                 }
             }
-            // left
-            if (height[rightMax] >= height[left]) {
-                if (height[left] >= height[leftMax] + (left - leftMax - 1)) {
-                    leftMax = left;
-                }
-            } else if (height[leftMax] < height[right]) {
-                if (height[rightMax] > height[leftMax] + (left - leftMax - 1)) {
-                    leftMax = left;
-                }
-            }
-            left++;
-            right--;
+            left--;
+            right++;
         }
 
         if (height[leftMax] > height[rightMax]) {
