0392e63b50441c3c7b3b06fe1ef8a9ce931a148d
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-01-08 22:56:59 +0530

    Sum Closest 3 tring

---
+++ SumClosestQ16.java
@@ -0,0 +1,13 @@
+public class SumClosestQ16 {
+    public static void main(String[] args) {
+        int[] nums = { -1, 2, 1, -4 };
+        int target = 10;
+        System.out.println(threeSumClosest(nums, target));
+
+    }
+
+    public static int threeSumClosest(int[] nums, int target) {
+
+        return 1;
+    }
+}
