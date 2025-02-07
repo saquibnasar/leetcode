a57c9902399c177e06d986abb41424502326c002
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-02-07 09:51:33 +0530

    four sum using recursion

--- FourSumQ18.java
+++ FourSumQ18.java
@@ -4,45 +4,55 @@ import java.util.List;
 
 public class FourSumQ18 {
     public static void main(String[] args) {
-        int[] nums = { 1000000000, 1000000000, 1000000000, 1000000000 };
-        System.out.println(1000000000 + 1000000000 + 1000000000 + 1000000000);
+        int[] nums = { -2, -1, 0, 0, 1, 2 };
 
-        System.out.println(fourSum(nums, -294967296));
+        System.out.println(fourSum(nums, 0));
     }
 
     public static List<List<Integer>> fourSum(int[] nums, int target) {
-        // if (-294967296 == target || target == 294967296)
-        // return new ArrayList<>();
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                int left = j + 1;
-                int right = nums.length - 1;
-                if (left == j || right == j)
-                    continue;
-
-                while (left < right) {
-                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
-                    if (result.contains(Arrays.asList(nums[i], nums[j], nums[left], nums[right]))) {
-                        break;
-                    }
-                    if (sum > target) {
-                        right--;
-                    } else if (sum < target) {
-                        left++;
-
-                    } else if (sum == target) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        left++;
-                        while (nums[left] == nums[left - 1] && left < right) {
-                            left++;
-                        }
-                    }
-                }
-
-            }
-        }
+        // using recusion
+        KSumRecursion(nums, target);
         return result;
     }
+
+    public static List<List<Integer>> KSumRecursion(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        return result;
+    }
+    // public static List<int[]> sum3(int[] nums) {
+
+    // if (nums == null || nums.length < 3)
+    // return new ArrayList<>();
+
+    // Arrays.sort(nums);
+    // List<int[]> li = new ArrayList<>();
+    // for (int i = 0; i < nums.length - 2; i++) {
+    // if (i > 0 && nums[i] == nums[i - 1]) {
+    // continue;
+    // }
+    // int left = i + 1;
+    // int right = nums.length - 1;
+
+    // while (left < right) {
+    // int sum = nums[i] + nums[left] + nums[right];
+    // if (sum > 0) {
+    // right--;
+    // } else if (sum < 0) {
+    // left++;
+    // } else {
+    // li.add(new int[] { nums[i], nums[left], nums[right] });
+    // left++;
+    // while (nums[left] == nums[left - 1] && left < right) {
+    // left++;
+    // }
+    // }
+
+    // }
+    // }
+    // return li;
+
+    // }
 }
