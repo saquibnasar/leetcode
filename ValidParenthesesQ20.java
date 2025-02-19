291ae3a30d30d056a8b83e2503a9965e927a0a07
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-02-19 18:44:21 +0530

    valid parentheses  using hashmap and arraylist

--- ValidParenthesesQ20.java
+++ ValidParenthesesQ20.java
@@ -1,111 +1,41 @@
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 public class ValidParenthesesQ20 {
     public static void main(String[] args) {
-        String s = "([])";
+        String s = "({{{{}}}))";
         boolean ans = isValid(s);
         System.out.println(ans);
     }
 
     public static boolean isValid(String s) {
-        if (s.length() == 0) {
-            return true;
-        }
+
+        List<Character> stack = new ArrayList<>();
+        LinkedHashMap<Character, Character> closeToOpen = new LinkedHashMap<>();
+
+        closeToOpen.put(')', '(');
+        closeToOpen.put(']', '[');
+        closeToOpen.put('}', '{');
+
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j < s.length(); j++) {
-                if (s.charAt(i) == '(' && s.charAt(j) == ')') {
-                    break;
-                } else if (s.charAt(i) == '{' && s.charAt(j) == '}') {
-                    break;
-                } else if (s.charAt(i) == '[' && s.charAt(j) == ']') {
-                    break;
-                } else if (s.charAt(i) == '(' && s.charAt(j) == '}') {
-                    for (int a = i + 1; a < j; a++) {
-                        if (s.charAt(a) == '{') {
-                            continue;
-                        }
-                    }
-                    return false;
-
-                } else if (s.charAt(i) == '(' && s.charAt(j) == ']') {
-                    for (int a = i; a < j; a++) {
-                        if (s.charAt(a) == '[') {
-                            continue;
-                        }
-                    }
-                    return false;
-                } else if (s.charAt(i) == '[' && s.charAt(j) == '}') {
-                    for (int a = i; a < j; a++) {
-                        if (s.charAt(a) == '{') {
-                            continue;
-                        }
-
-                    }
-                    return false;
-                } else if (s.charAt(i) == '[' && s.charAt(j) == ')') {
-                    for (int a = i; a < j; a++) {
-                        if (s.charAt(a) == '(') {
-                            continue;
-                        }
-
-                    }
-                    return false;
-                } else if (s.charAt(i) == '{' && s.charAt(j) == ']') {
-                    for (int a = i; a < j; a++) {
-                        if (s.charAt(a) == '[') {
-                            continue;
-                        }
-
-                    }
-                    return false;
-
-                } else if (s.charAt(i) == '{' && s.charAt(j) == ')') {
-                    for (int a = i; a < j; a++) {
-                        if (s.charAt(a) == '(') {
-                            continue;
-                        }
-                    }
+            if (closeToOpen.containsKey(s.charAt(i))) {
+                if (!stack.isEmpty() && stack.get(stack.size() - 1) == closeToOpen.get(s.charAt(i))) {
+                    stack.remove(stack.size() - 1);
+                } else {
                     return false;
                 }
+            } else {
+                stack.add(s.charAt(i));
 
             }
 
         }
+        if (stack.size() == 0) {
+            return true;
+        }
 
-        return true;
+        return false;
     }
-    // public static boolean isValid(String s) {
-    // int findPoint = 0;
-    // if (s.length() == 0) {
-    // return true;
-    // }
-    // boolean ans = true;
-    // for (int i = 0; i < s.length(); i++) {
 
-    // for (int j = i + 1; j < s.length(); j++) {
-    // if (s.charAt(i) == '(') {
-    // if (s.charAt(j) == ')') {
-    // ans = true;
-    // break;
-    // }
-    // } else if (s.charAt(i) == '{') {
-    // if (s.charAt(j) == '}') {
-    // ans = true;
-    // break;
-    // }
-    // } else if (s.charAt(i) == '[') {
-    // if (s.charAt(j) == ']') {
-    // ans = true;
-    // break;
-    // }
-    // } else {
-    // break;
-    // }
-    // ans = false;
-    // }
-    // if (!ans) {
-    // break;
-    // }
-
-    // }
-    // return ans;
-    // }
 }
