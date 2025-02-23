2f5c06da6f3cb4dcb325e64c380363a485ba1d4d
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-02-23 18:24:37 +0530

    new way to solve this question

--- RemoveDuplicatesfromSortedArrayQ26.java
+++ RemoveDuplicatesfromSortedArrayQ26.java
@@ -1,52 +1,50 @@
 public class RemoveDuplicatesfromSortedArrayQ26 {
     public static void main(String[] args) {
-        int[] arr = { 1, 1 };
+        int[] arr = { 1, 2, 2, 3 };
         System.out.println(removeDuplicates(arr));
 
     }
 
     public static int removeDuplicates(int[] nums) {
         int k = 1;
-        int max = 0;
         for (int j = 1; j < nums.length; j++) {
-            if (nums[j] == nums[j - 1]) {
-                int i = j + 1;
-                if (i < nums.length) {
-                    while (nums[max] >= nums[i]) {
-                        i++;
-                    }
-                    nums[max + 1] = nums[i];
-                    max++;
-                    j = i;
-                    k++;
-                }
+            if (nums[j] != nums[j - 1]) {
+                nums[k] = nums[j];
+                k++;
             }
-
         }
         return k;
     }
+
     // public static int removeDuplicates(int[] nums) {
     // int k = 1;
     // int max = 0;
     // for (int j = 1; j < nums.length; j++) {
     // if (nums[j] == nums[j - 1]) {
-    // if (nums[max] < nums[j + 1]) {
-    // nums[max + 1] = nums[j + 1];
-    // max++;
-    // k++;
-    // } else {
     // int i = j + 1;
+    // if (i == nums.length) {
+    // return k;
+    // }
     // while (nums[max] >= nums[i]) {
     // i++;
+    // if (i == nums.length) {
+    // return k;
+    // }
     // }
     // nums[max + 1] = nums[i];
     // max++;
     // j = i;
     // k++;
+    // } else {
+    // k++;
+    // if (nums[max] < nums[j]) {
+    // nums[max + 1] = nums[j];
     // }
+    // max++;
     // }
 
     // }
     // return k;
     // }
+
 }
