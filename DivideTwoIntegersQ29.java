0bf92d3e42f3f41434cd9c9ba60c18b16f7c9910
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-03-06 06:17:53 +0530

    Refactor divide method in DivideTwoIntegersQ29 to handle negative values and improve logic

--- DivideTwoIntegersQ29.java
+++ DivideTwoIntegersQ29.java
@@ -1,7 +1,7 @@
 public class DivideTwoIntegersQ29 {
     public static void main(String[] args) {
-        int dividend = 10;
-        int divisor = 3;
+        int dividend = -2147483647;
+        int divisor = -1;
         System.out.println(divide(dividend, divisor));
 
     }
@@ -9,22 +9,50 @@ public class DivideTwoIntegersQ29 {
     public static int divide(int dividend, int divisor) {
         int ans = 0;
         int extraValue = 0;
-
+        int originalDivisor = divisor;
+        int originalDividend = dividend;
+        if (dividend < 0 && divisor > 0 || dividend > 0 && divisor < 0 || dividend < 0 && divisor < 0) {
+            // find way to make it positive without changing the sign
+            divisor = Math.abs(divisor);
+            dividend = Math.abs(dividend);
+        }
         int currentPointer = 0;
         String dividendStr = String.valueOf(dividend);
 
         while (currentPointer < dividendStr.length()) {
-            int currentMath = Integer.parseInt(dividendStr.substring(currentPointer, currentPointer));
-            if (extraValue == 0) {
-                currentMath = Integer.parseInt(dividendStr.substring(currentPointer, currentPointer + 1));
+            int currentMath = Integer.parseInt(dividendStr.substring(currentPointer, currentPointer + 1));
+            if (extraValue <= 0 && currentPointer + 1 < dividendStr.length()) {
+                currentMath = Integer.parseInt(dividendStr.substring(currentPointer, currentPointer + 2));
+            }
+            if (extraValue > 0) {
+                currentMath = extraValue * 10 + currentMath;
             }
 
-            while (currentMath < divisor) {
+            while (currentMath >= divisor) {
+                currentMath = currentMath - divisor;
+                ans++;
+                if (currentMath - divisor < 0) {
+                    extraValue = currentMath;
+                    break;
+                }
 
             }
+            if (extraValue > 0) {
+                currentPointer += 2;
+            } else {
+                currentPointer += 3;
+            }
+            if (currentPointer < dividendStr.length()) {
+                ans = ans * 10;
+            }
+
         }
 
-        return dividend / divisor;
+        if (originalDividend < 0 && originalDivisor > 0 || originalDividend > 0 && originalDivisor < 0) {
+            ans = -ans;
+        }
+
+        return ans;
     }
 
 }
