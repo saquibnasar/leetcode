261a0860a26e32a57fc1e34c388b7c97d5bbcc54
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-03-11 10:05:06 +0530

    i tried to solve this problem using the concept of taking first two digits then subtracting them from the dividend and dividing the result by the  divisor and adding 1 to the answer

--- DivideTwoIntegersQ29.java
+++ DivideTwoIntegersQ29.java
@@ -1,81 +1,88 @@
 public class DivideTwoIntegersQ29 {
     public static void main(String[] args) {
-        int dividend = 2147483647;
-        int divisor = 2;
+        int dividend = -1010369383;
+        int divisor = -2147483648;
 
         System.out.println(divide(dividend, divisor));
 
     }
 
     public static int divide(int dividend, int divisor) {
-        if ((dividend <= Integer.MIN_VALUE) && (divisor == -1)) {
-            return Integer.MAX_VALUE;
-        }
-        int maxInt = 2147483647;
-        int minInt = -2147483648;
-
-        int ans = 0;
-        String extraValue = "";
-        int originalDivisor = divisor;
-        int originalDividend = dividend;
-        // if (dividend == minInt) {
-        // dividend = maxInt;
-        // }
-        // if (dividend < 0 && divisor > 0 || dividend > 0 && divisor < 0 || dividend <
-        // 0 && divisor < 0) {
-        // // find way to make it positive without changing the sign
-        // divisor = Math.abs(divisor);
-        // dividend = Math.abs(dividend);
-        // }
-        divisor = Math.abs(divisor);
-        int currentPointer = 0;
-        if (dividend < 0) {
-            currentPointer = 1;
-        }
-        String dividendStr = String.valueOf(dividend);
-
-        while (currentPointer < dividendStr.length()) {
-            int currentMath = Integer.parseInt(dividendStr.substring(currentPointer, currentPointer + 1));
-            if (extraValue.length() <= 0 && currentPointer + 1 < dividendStr.length()) {
-                currentMath = Integer.parseInt(dividendStr.substring(currentPointer, currentPointer + 2) + extraValue);
-                currentPointer++;
-            }
-            int currentans = 0;
-            boolean isNegative = true;
-            while (currentMath >= divisor) {
-
-                currentMath = currentMath - divisor;
-                currentans++;
-                ans++;
-                if (currentMath - divisor < 0 && currentMath % divisor == 1) {
-                    String temp = String.valueOf(currentMath);
-                    extraValue = temp;
-                    break;
-                }
-                if (currentans + 1 > 9 && isNegative && currentPointer > 1) {
-                    String temp = String.valueOf(ans);
-                    String temp2 = temp.substring(temp.length() - 1, temp.length());
-                    temp = temp.substring(0, temp.length() - 1);
-                    temp = temp + "0" + temp2;
-                    ans = Integer.parseInt(temp);
-                    isNegative = false;
-                }
-
-            }
-            currentPointer++;
-            if (currentPointer < dividendStr.length()) {
-                String temp = String.valueOf(ans);
-                temp = temp + "0";
-                ans = Integer.parseInt(temp);
-            }
-
-        }
-
-        if (originalDividend < 0 && originalDivisor > 0 || originalDividend > 0 && originalDivisor < 0) {
-            ans = -ans;
-        }
-
-        return ans;
+        return 0;
     }
+
+    // i tried to solve this problem using the concept of taking first two digits
+    // then subtracting them from the dividend and dividing the result by the
+    // divisor and adding 1 to the answer
+
+    // public static int divideFirstTwoDigits(int dividend, int divisor) {
+    // if ((dividend <= Integer.MIN_VALUE) && (divisor == -1)) {
+    // return Integer.MAX_VALUE;
+    // }
+    // if ((dividend <= Integer.MIN_VALUE)) {
+    // return Integer.MIN_VALUE;
+    // }
+    // int ans = 0;
+    // String extraValue = "";
+    // int originalDivisor = divisor;
+    // int originalDividend = dividend;
+
+    // divisor = Math.abs(divisor);
+    // int currentPointer = 0;
+    // if (dividend < 0) {
+    // currentPointer = 1;
+    // }
+    // String dividendStr = String.valueOf(dividend);
+
+    // while (currentPointer < dividendStr.length()) {
+    // int currentMath = Integer.parseInt(extraValue +
+    // dividendStr.substring(currentPointer, currentPointer + 1));
+    // if (extraValue.length() <= 0 && currentPointer + 1 < dividendStr.length()) {
+    // currentMath = Integer.parseInt(dividendStr.substring(currentPointer,
+    // currentPointer + 2));
+    // currentPointer++;
+    // }
+    // int currentans = 0;
+    // boolean isNegative = true;
+    // while (currentMath >= divisor) {
+
+    // currentMath = currentMath - divisor;
+    // currentans++;
+    // ans++;
+    // if (currentMath - divisor < divisor && currentMath - divisor > 0) {
+    // currentMath = currentMath - divisor;
+    // ans++;
+    // String temp = String.valueOf(currentMath);
+    // extraValue = temp;
+    // break;
+    // } else {
+    // extraValue = "";
+    // }
+    // if (currentans + 1 > 9 && isNegative && currentPointer > 1) {
+    // String temp = String.valueOf(ans);
+    // String temp2 = temp.substring(temp.length() - 1, temp.length());
+    // temp = temp.substring(0, temp.length() - 1);
+    // temp = temp + "0" + temp2;
+    // ans = Integer.parseInt(temp);
+    // isNegative = false;
+    // }
+
+    // }
+    // currentPointer++;
+    // if (currentPointer < dividendStr.length()) {
+    // String temp = String.valueOf(ans);
+    // temp = temp + "0";
+    // ans = Integer.parseInt(temp);
+    // }
+
+    // }
+
+    // if (originalDividend < 0 && originalDivisor > 0 || originalDividend > 0 &&
+    // originalDivisor < 0) {
+    // ans = -ans;
+    // }
+
+    // return ans;
+    // }
 
 }
