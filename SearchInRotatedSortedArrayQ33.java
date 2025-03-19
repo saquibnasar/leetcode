ee2b493c0dd9b3e05026025e93b5e6b1473b0067
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-03-19 11:34:28 +0530

    Refactor search method in SearchInRotatedSortedArrayQ33 to improve pivot finding logic and handle edge cases; update main method for testing

--- SearchInRotatedSortedArrayQ33.java
+++ SearchInRotatedSortedArrayQ33.java
@@ -1,35 +1,61 @@
 public class SearchInRotatedSortedArrayQ33 {
     public static void main(String[] args) {
-        int[] nums = { 2, 1 };
-        System.out.println(search(nums, 1));
+        int[] nums = { 1, 3, };
+        System.out.println(search(nums, 3));
     }
 
     public static int search(int[] nums, int target) {
+
+        if (nums.length <= 1) {
+            if (nums.length == 1 && nums[0] == target) {
+                return 0;
+            }
+
+            return -1;
+        }
 
         // find the pivot
         // then find the
         int start = 0;
         int end = nums.length - 1;
-        int mid = start + (end - start) / 2;
+        int pivot = -1;
 
-        // find the pivot
-        if (nums[mid] < nums[mid - 1]) {
-            if (target <= nums[nums.length - 1]) {
-                start = mid;
-            } else {
-                end = mid;
-            }
-        } else if (nums[mid] > nums[mid + 1]) {
-            if (target >= nums[0]) {
-                end = mid;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid + 1]) {
 
-            } else {
+                if (nums[mid] > nums[mid + 1]) {
+                    if (target >= nums[0]) {
+                        end = mid;
+
+                    } else {
+                        start = mid + 1;
+                    }
+                } else if (mid != 0 && nums[mid] < nums[mid - 1]) {
+                    if (target <= nums[nums.length - 1]) {
+                        start = mid;
+                    } else {
+                        end = mid;
+                    }
+                }
+                pivot = 1;
+                break;
+            } else if (nums[mid] > nums[0]) {
                 start = mid + 1;
+            } else if (nums[mid] > nums[nums.length - 1]) {
+                end = mid - 1;
+            } else {
+                break;
             }
+
+        }
+        if (pivot == -1) {
+            start = 0;
+            end = nums.length - 1;
         }
 
-        while (start <= end) {
-            mid = start + (end - start) / 2;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
 
             // if the target is less than the mid, then the target is in the left half
             if (nums[mid] < target) {
