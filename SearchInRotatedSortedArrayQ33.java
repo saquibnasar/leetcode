f8b04fa2981355c47bc2d935ccca881e2d3e461f
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-03-22 11:07:03 +0530

    Refactor search method in SearchInRotatedSortedArrayQ33 to enhance binary search logic for improved target finding; handle edge cases for both sorted portions

--- SearchInRotatedSortedArrayQ33.java
+++ SearchInRotatedSortedArrayQ33.java
@@ -5,6 +5,38 @@ public class SearchInRotatedSortedArrayQ33 {
     }
 
     public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // left sorted portion
+            if (nums[left] <= nums[mid]) {
+                if (target > nums[mid] || target < nums[left]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+
+            }
+            // right sorted portion
+            else {
+                if (target < nums[mid] || target > nums[right]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+        }
+
+        return -1;
+
+    }
+
+    public static int searchMyAnswer(int[] nums, int target) {
 
         if (nums.length <= 1) {
             if (nums.length == 1 && nums[0] == target) {
