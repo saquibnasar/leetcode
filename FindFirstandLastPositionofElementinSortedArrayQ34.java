21c98bec3e0471df4d61f9cf13053e77474b8f41
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-03-27 10:47:50 +0530

    Refactor searchRange method in FindFirstandLastPositionofElementinSortedArrayQ34 to improve target finding logic; enhance first and last occurrence detection and update main method for testing with new input

--- FindFirstandLastPositionofElementinSortedArrayQ34.java
+++ FindFirstandLastPositionofElementinSortedArrayQ34.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
+
 public class FindFirstandLastPositionofElementinSortedArrayQ34 {
     public static void main(String[] args) {
-        int[] arr = { 5, 7, 7, 8, 8, 8, 8, 10, 11 };
-        System.out.println(searchRange(arr, 5));
+        int[] arr = { 1, 2, 3 };
+        System.out.println(Arrays.toString(searchRange(arr, 2)));
     }
 
     public static int[] searchRange(int[] nums, int target) {
         if (nums.length == 0)
             return new int[] { -1, -1 };
-        int[] ans = new int[2];
+        int[] ans = new int[] { -1, -1 };
 
         int left = 0;
         int right = nums.length - 1;
@@ -15,13 +17,45 @@ public class FindFirstandLastPositionofElementinSortedArrayQ34 {
             int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
-                if (left != target) {
-                    left = left + 1;
-                }
-                if (right != target) {
-                    right = right - 1;
-                }
 
+                // first occurrence
+                while (left <= right) {
+                    mid = left + (right - left) / 2;
+
+                    // if (nums[mid] == target) {
+                    // if (nums[mid] == target && nums[mid - 1] > target) {
+                    if (nums[mid] == target && (mid > 0 || nums[mid - 1] > target)) {
+                        ans[0] = mid;
+                        break;
+                    }
+                    if (nums[mid] < target) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+
+                }
+                if (ans[0] == -1)
+                    ans[0] = mid;
+
+                left = mid + 1;
+                right = nums.length - 1;
+                while (left <= right) {
+                    mid = left + (right - left) / 2;
+
+                    if (nums[mid] == target && (mid == nums.length - 1 || nums[mid + 1] > target)) {
+                        ans[1] = mid;
+                        return ans;
+                    }
+                    if (nums[mid] <= target) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+
+                }
+                ans[1] = ans[0];
+                return ans;
             } else if (nums[mid] > target) {
                 right = mid - 1;
             } else {
