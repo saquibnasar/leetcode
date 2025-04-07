610c443447b29b0b1027fce22b4db08b2d398303
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-04-07 10:38:17 +0530

    Implement searchInsert method in SearchInsertPosition class to determine the index for target insertion in a sorted array; update main method for testing with new input

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -1,12 +1,25 @@
 public class SearchInsertPosition {
     public static void main(String[] args) {
-        int[] nums = { 1, 3, 5, 6 };
-        int target = 5;
+        int[] nums = {};
+        int target = 7;
         System.out.println(searchInsert(nums, target));
     }
 
     public static int searchInsert(int[] nums, int target) {
-        return -1;
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
 
 }
