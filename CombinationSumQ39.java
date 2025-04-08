75f352b3a02ce5f6552ac5a3783e6f7a413a9085
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-04-08 22:11:15 +0530

    Add CombinationSumQ39 class with main method and initial combinationSum method stub

---
+++ CombinationSumQ39.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class CombinationSumQ39 {
+    public static void main(String[] args) {
+        int[] candidates = { 2, 3, 6, 7 };
+        int target = 7;
+        System.out.println(combinationSum(candidates, target));
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        return null;
+    }
+}
