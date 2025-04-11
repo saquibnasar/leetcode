6b4a73a08519623ac75657e663651f2ad939c50a
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-04-11 23:25:58 +0530

    Refactor combinationSum method by removing commented-out code and simplifying logic for handling target zero case.

--- CombinationSumQ39.java
+++ CombinationSumQ39.java
@@ -10,22 +10,7 @@ public class CombinationSumQ39 {
 
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        // if (candidates.length == 0) {
-        // return result;
-        // }
-        if (target == 0) {
-            result.add(new ArrayList<>());
-            return result;
-        }
-        for (int i = 0; i < candidates.length; i++) {
-            if (candidates[i] > target) {
-                break;
-            }
-            List<Integer> temp = new ArrayList<>();
-            temp.add(candidates[i]); // add the current candidate
-            result.addAll(combinationSum(candidates, target - candidates[i]));
-            result.addAll(combinationSum(candidates, target));
-        }
+
         return result;
     }
 }
