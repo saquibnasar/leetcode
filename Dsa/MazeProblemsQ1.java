2f4f0b540b3b5b2ad9472285671885b1daf2f0c7
saquibnasar <saquibsiddiqui023@gmail.com>
2025-05-17 23:39:14 +0530

    Refactor findPath method to improve pathfinding logic and remove unnecessary code

--- Dsa/MazeProblemsQ1.java
+++ Dsa/MazeProblemsQ1.java
@@ -8,15 +8,22 @@ public class MazeProblemsQ1 {
     }
 
     public static String[] findPath(int r, int c, String p) {
+
+        if (r == 1 && c == 1) {
+
+            System.out.println(p);
+            return new String[] { p };
+        }
         if (r == 1 || c == 1) {
             return new String[] { p };
         }
+        if (r > 1) {
+            String[] right = findPath(r - 1, c, p + "R");
+        }
 
-        String[] right = findPath(r - 1, c, p + "R");
+        String[] left = findPath(r, c - 1, p + "L");
 
-        String[] left = findPath(r, c - 1, p + "D");
-        System.out.println(right[0] + " " + left[0]);
-        return new String[] { right[0], left[0] };
+        return new String[] { left[0] };
     }
 
     public static int findPathcount(int r, int c) {
